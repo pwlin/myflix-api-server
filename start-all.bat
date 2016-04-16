4cd288eff2ed4956fee1aab35c3c302bfd069b41
pwlin <pwlin05@gmail.com>
2016-04-16 02:25:10 +0200

    Addeing 3 seconds sleep

--- start-all.bat
+++ start-all.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
 CALL KILLALL node > nul 2> nul
+CALL SLEEP 3
 CALL DEL %~dp0db\db.sqlite > nul 2> nul 
+CALL SLEEP 3
 CALL node start.js thumbs 
 CALL node start.js aggregate
 CALL node start.js
--- start-serve.bat
+++ start-serve.bat
@@ -1,3 +1,4 @@
 @ECHO OFF
 CALL KILLALL node > nul 2> nul
+CALL SLEEP 3
 CALL node start.js
